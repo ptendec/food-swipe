f604deaf0aff3f99eb7ba1064b64db23bd5ab016
texta-ptendec <sherkhan@texta.ai>
2022-05-04 01:32:50 +0600

    Main page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {store} from 'store'
+import {store} from './store'
 import {BrowserRouter as Router} from "react-router-dom"
 import {Provider} from "react-redux"
 
